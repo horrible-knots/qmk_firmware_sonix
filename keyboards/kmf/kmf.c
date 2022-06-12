d5a0c60627bdcef63913f5a3b1b955c8c27951e5
andy <andy@andy-laptop2>
2022-06-11 20:20:46 -0400

    ok

---
+++ keyboards/kmf/kmf.c
@@ -0,0 +1,59 @@
+// Copyright 2022 nobody (@nobody)
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#include "eeconfig.h"
+#include "kmf.h"
+#include "config.h"
+#include "keymaps/default/keymap.h"
+#include "config_led.h"
+#include "openrgb_eeprom.h"
+
+#define GAMER_MODE_LED_BIT 31
+
+int gamer_mode_led_state = !LED_PIN_ON_STATE;
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record);
+void keyboard_post_init_user(void);
+void gamer_mode_state_to_eeprom(int state);
+extern void matrix_init(void);
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+#ifdef CONSOLE_ENABLE
+       uprintf("KL: kc: 0x%04X, col: %u, row: %u, pressed: %b, time: %u, interrupt: %b, count: %u\n", 
+           keycode, record->event.key.col, record->event.key.row, record->event.pressed, record->event.time, record->tap.interrupted, record->tap.count);
+#endif
+
+    switch (keycode) {
+        case GAMER_MODE:
+            // Don't handle the Windows key if Function-Windows has been pressed
+            if (record->event.pressed) {
+                gamer_mode_led_state ^= 1;
+                writePin(LED_GAMER_MODE_PIN, gamer_mode_led_state);
+                gamer_mode_state_to_eeprom(gamer_mode_led_state);
+		return false;
+            }
+	case KC_LWIN:
+	    return !(gamer_mode_led_state == LED_PIN_ON_STATE);
+        case KC_APP:
+            return !(gamer_mode_led_state == LED_PIN_ON_STATE);
+    }
+    return true;
+}
+
+void gamer_mode_state_to_eeprom(int state) {
+    uint32_t eedata = eeconfig_read_user();
+    eedata &= ~(1 << GAMER_MODE_LED_BIT);
+    eedata |= (state << GAMER_MODE_LED_BIT);
+    eeconfig_update_user(eedata);
+}
+
+void keyboard_post_init_user(void) {
+    setPinOutput(LED_GAMER_MODE_PIN);
+    uint32_t eedata = eeconfig_read_user();
+    gamer_mode_led_state = ((eedata >> GAMER_MODE_LED_BIT) & 1);
+    writePin(LED_GAMER_MODE_PIN, gamer_mode_led_state);    
+#ifdef OPENRGB_ENABLE
+    openrgb_load_rgb_state_eeprom();
+#endif
+}
+
---
+++ keyboards/kmf/rules.mk
@@ -0,0 +1,42 @@
+# project specific files
+SRC = ../../../drivers/led/sn32/matrix_sn32f24xx.c
+SRC += config_led.c
+SRC += bootmagic_lite.c
+SRC += command_extra.c
+SRC += openrgb_eeprom.c
+
+# MCU name - Engraved with VS11K09A-1.
+MCU = SN32F248BF
+
+# Build Options
+#   comment out to disable the options.
+#
+LTO_ENABLE = no
+BACKLIGHT_ENABLE = no
+MOUSEKEY_ENABLE = no    # Mouse keys
+EXTRAKEY_ENABLE = yes   # Audio control and System control
+SLEEP_LED_ENABLE = no   # Breathing sleep LED during USB suspend
+VIRTSER_ENABLE = no
+NKRO_ENABLE = yes       # USB Nkey Rollover
+AUDIO_ENABLE = no
+WAIT_FOR_USB = no
+KEYBOARD_SHARED_EP = yes
+DEBOUNCE_TYPE = sym_eager_pk
+# Debug
+MAGIC_KEYCODE_ENABLE = yes # See quantum/command.c and quantum/command.h for available commands
+CONSOLE_ENABLE = no        # Console for debug.
+COMMAND_ENABLE = yes       # Commands for debug and configuration.
+BOOTMAGIC_ENABLE = yes     # Hold down escape while powering up to jump to bootloader.  Seems broken currently.
+# Choose which key to start up in bootloader by matrix grid.
+BOOTMAGIC_LITE_COLUMN = 0
+BOOTMAGIC_LITE_ROW    = 0
+# Print out uS per matrix scan when CONSOLE_ENABLE = yes on the HID listener.
+DEBUG_MATRIX_SCAN_RATE = no
+# OpenRGB
+OPENRGB_ENABLE = no
+OPENRGB_DIRECT_MODE_UNBUFFERED = no
+
+# Custom RGB matrix handling
+CUSTOM_MATRIX = yes
+RGB_MATRIX_ENABLE = yes
+RGB_MATRIX_DRIVER = SN32F24xB
