f7c8c662d9472669a714a01cff5f4f237f6f8035
andy <epicwrathssin@yandex.ru>
2022-08-14 02:47:03 -0400

    tapdance test

--- tmk_core/protocol/usb_descriptor.h
+++ tmk_core/protocol/usb_descriptor.h
@@ -200,7 +200,7 @@ enum usb_interfaces {
  * Endpoint numbers
  */
 enum usb_endpoints {
-    __unused_epnum__ = NEXT_EPNUM,  // Endpoint numbering starts at 1
+    __unused_epnum__ = NEXT_EPNUM, // Endpoint numbering starts at 1
 
 #ifndef KEYBOARD_SHARED_EP
     KEYBOARD_IN_EPNUM = NEXT_EPNUM,
@@ -302,7 +302,7 @@ enum usb_endpoints {
 #define KEYBOARD_EPSIZE 8
 #define SHARED_EPSIZE 32
 #define MOUSE_EPSIZE 8
-#define RAW_EPSIZE 64
+#define RAW_EPSIZE 32
 #define CONSOLE_EPSIZE 32
 #define MIDI_STREAM_EPSIZE 64
 #define CDC_NOTIFICATION_EPSIZE 8
